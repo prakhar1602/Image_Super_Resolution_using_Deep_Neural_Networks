993ae5e7b6d3fdd58900366e10cc9ba64b154607
prakhar1602 <42375923+prakhar1602@users.noreply.github.com>
2021-04-16 10:03:51 -0400

    parametrizing kernel_size

--- srcnnModel.py
+++ srcnnModel.py
@@ -1,11 +1,11 @@
 from torch import nn
 
 class srcnnModel(nn.Module):
-	def __init__(self, num_channels=1):
+	def __init__(self, num_channels=1, kernel_size_l1 = 9, kernel_size_l2=5, kernel_size_l3=5):
 		super(srcnnModel, self).__init__()
-		self.layer1 = nn.Conv2d(num_channels, 64, kernel_size=9, padding = 9//2)
-		self.layer2 = nn.Conv2d(64, 32, kernel_size=5, padding = 5//2)
-		self.layer3 = nn.Conv2d(32, num_channels, kernel_size=5, padding = 5//2)
+		self.layer1 = nn.Conv2d(num_channels, 64, kernel_size=kernel_size_l1, padding = kernel_size_l1//2)
+		self.layer2 = nn.Conv2d(64, 32, kernel_size=kernel_size_l2, padding = kernel_size_l2//2)
+		self.layer3 = nn.Conv2d(32, num_channels, kernel_size=kernel_size_l3, padding = kernel_size_l3//2)
 		self.relu = nn.Relu(inplace=True)
 
 	def forward(self, x):
