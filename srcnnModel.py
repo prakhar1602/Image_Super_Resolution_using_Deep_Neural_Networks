a8aca7f51c350c481afe0832be0b4058f724f46f
prakhar1602 <42375923+prakhar1602@users.noreply.github.com>
2021-04-20 02:10:57 -0400

    Update srcnnModel.py

--- srcnnModel.py
+++ srcnnModel.py
@@ -4,12 +4,10 @@ from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.layers import Activation
 
 class srcnn(Model):
-	def __init__(self, img_size, classes, optimizer, loss, metric = ['accuracy']):
+	def __init__(self, img_size, classes):
 		super(srcnn, self).__init__()
 		self.img_size = img_size
 		self.classes = classes
-		self.optimizer = optimizer
-		self.metric = metric
 		
 		self.conv1 = Conv2D(64,9,padding='same', input_shape = (self.img_size, self.img_size, self.classes), activation='relu')
 		self.conv2 = Conv2D(32,1,padding='same', activation='relu')
